5e7945c74ea63165fb030d3a80f93f35671b67b3
Hoon Lee <yl.hoony@gmail.com>
2017-12-31 23:04:02 -0800

    tested rake db:migrate, rollback

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,23 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
+require_relative "../artist.rb"
 
 # put the code to connect to the database here
 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
-require_relative "../artist.rb"
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS artists (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+#   )
+# SQL
+#
+# ActiveRecord::Base.connection.execute(sql)
